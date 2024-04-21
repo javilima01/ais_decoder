10d43734a3ffc648e97e139ab6664bb68fadbf71
javilima01 <javierbilbao75@gmail.com>
2024-04-21 13:57:58 +0200

    JaBi: email is required to post coordinates

--- ais_receiver.py
+++ ais_receiver.py
@@ -21,32 +21,23 @@ if __name__ == "__main__":
     parser.add_argument("--port", type=int, default=10110, help="UDP server port")
     parser.add_argument("--endpoint", type=str, default="svais.jbl.mooo.com/api/coords/coords", help="API endpoint")
     parser.add_argument("--protocol", choices=["http", "https"], default="https", help="HTTP protocol")
-    parser.add_argument("--token", type=str, default="token", help="Authentication token")
-    parser.add_argument("--token-file", type=str, help="File containing the authentication token")
+    parser.add_argument("--email", type=str, help="Authentication token")
     args = parser.parse_args()
 
-    if args.token and args.token_file:
-        print("Error: Please provide either a token or a token file, not both.")
-        exit(1)
-
-    if args.token_file:
-        auth_token = read_token_from_file(args.token_file)
-    else:
-        auth_token = args.token
-
+    if not args.email:
+        print('Please provide a valid email.')
+        exit()
+        
     url = f"{args.protocol}://{args.endpoint}"
 
-    headers = {
-        'Authorization': 'Bearer ' + auth_token,
-        'Content-Type': 'application/json'
-    }
 
     for msg in UDPReceiver(args.host, port=args.port):
         msg = msg.encode('utf-8')
         msg = {
+            'email': args.email,
             'nmea': msg
         }
-        response = requests.post(url, json=msg, headers=headers)
+        response = requests.post(url, json=msg)
         if response.status_code == 200:
             print('POST request was successful!')
         else:
